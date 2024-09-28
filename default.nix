7599c85c2674345683756c2c4239c62e748301ee
Spaceplone <72419230+Spaceplone@users.noreply.github.com>
2024-09-28 03:42:57 +0200

    initial commit: added default.nix for vban on nixos

---
+++ default.nix
@@ -0,0 +1,31 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.stdenv.mkDerivation rec {
+  pname = "vban";
+  version = "2024-09-28";  # Update to the latest version or a specific commit
+  
+  src = pkgs.fetchFromGitHub {
+    owner = "quiniouben";
+    repo = "vban";
+    rev = "master";
+    sha256 = "sha256-V7f+jcj3NpxXNr15Ozx2is4ReeeVpl3xvelMuPNfNT0=";  
+  };
+  
+  nativeBuildInputs = [ pkgs.cmake pkgs.pkg-config ];
+  buildInputs = [ pkgs.cmake pkgs.pkg-config pkgs.alsaLib pkgs.pulseaudio.dev ];  # Include additional dependencies here
+
+  cmakeFlags = [
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DWITH_ALSA=Yes"
+    "-DWITH_PULSEAUDIO=Yes"
+    "-DWITH_JACK=No"  # Disable JACK backend
+  ];
+
+  meta = with pkgs.lib; {
+    description = "A C++ library and tools for VBAN protocol";
+    homepage = "https://github.com/quiniouben/vban";
+    license = licenses.gpl3Plus;
+    maintainers = [ ];
+    platforms = platforms.linux;
+  };
+}
